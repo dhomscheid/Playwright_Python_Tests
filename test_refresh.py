dc2c1404d82e924ad6554471599a5d88ae6cbf82
dhomscheid@gmail.com <dhomscheid@gmail.com>
2024-07-04 23:41:01 +0200

    Test with bc, more notes

--- test_refresh.py
+++ test_refresh.py
@@ -2,13 +2,17 @@
 #pip install pytest-playwright
 #playwright install
 
+#Create an Exe File
+#pip install pyinstaller
+#create exe command: pyinstaller test_refresh.py --onefile --windowed
+
 from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False,slow_mo=1000)
     context = browser.new_context(viewport={"width": 1920, "height": 1080})
     page = context.new_page()
-    page.goto('https://workspace.google.com/marketplace/search/word')
+    page.goto('http://localhost:8080/BC230/') #http://localhost:8080/BC230/ #https://workspace.google.com/marketplace/search/word
     
     i = 0
     while i < 10:
